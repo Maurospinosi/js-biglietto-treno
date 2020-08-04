855ce5161e8873c68db05cd736ea1af81792e2d5
Mauro <mauro.spinosi94@gmail.com>
2020-08-04 16:27:45 +0200

    Sconto

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Inserisci i kilometri
+var km = prompt("Inserisci i kilometri");
+// Inserisci l'età
+var eta = prompt("Inserisci l'età");
+// Calcolo del prezzo in base ai km
+var prezzo = 0.21 * km;
+console.log(prezzo);
+
+if (eta <= 18){
+  var sconto = ((prezzo * 20) / 100);
+}else (eta >= 65){
+  var sconto = ((prezzo * 40) / 100);
+}
+console.log(sconto)
