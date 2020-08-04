1f0618f842a79db82d01c3a3437dcf23429bd344
Mauro <mauro.spinosi94@gmail.com>
2020-08-04 16:55:48 +0200

    Sconto pt3

--- js/script.js
+++ js/script.js
@@ -5,11 +5,16 @@ var eta = prompt("Inserisci l'età");
 // Calcolo del prezzo in base ai km
 var prezzo = 0.21 * km;
 console.log(prezzo);
-
+// Calcolo sconto
 if (eta <= 18) {
   var sconto = ((prezzo * 20) / 100);
 } else if (eta >= 65) {
   var sconto = ((prezzo * 40) / 100);
 }
 console.log(sconto);
-document.getElementById('ticket').innerHTML = "Prezzo con sconto applicato :" + " " + (prezzo - sconto);
+// prezzo con sconto appliacto
+var prezzoScontato = (prezzo - sconto);
+console.log(prezzo - sconto);
+
+document.getElementById('ticket-s').innerHTML = "Prezzo con sconto applicato :" + " " + prezzoScontato + "€";
+document.getElementById('ticket').innerHTML = "Prezzo :" + " " + prezzo + "€";
